363f15f9ad313a56c36286c1e3642a5d49f27a4c
Imran-S-heikh <imransheikhsadi@gmail.com>
2021-03-26 16:14:01 +0600

    Component Functionality added

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const loader_utils = require('loader-utils');
 const fs = require('fs'); 
 
+var componentReg = new RegExp(/<[A-Z]([^\/>]*)+\s?\/>/,"g");
+var componentNameReg = /(?<=<)[A-Z]\w+/;
+var argumentReg = new RegExp(/[A-Za-z]\w+=(("|')([^("|')]*)("|')|\d+)/,"g");
+var templateReg = new RegExp(/{([A-za-z])\w+}/,"g");
 
-function replaceAll(html,components){
-    var regex = new RegExp(Object.keys(components).join("|"),"gi");
+function injectArgs(component,args) {
+    var parsedComp;
 
-    return html.replace(regex, function(matched){
-        return components[matched];
+    if(args){
+        parsedComp = component.replace(templateReg,(match)=>{
+            var key = match.replace('{','').replace('}','');
+            if(args[key]){
+                return args[key]
+            }
+        });
+    }else{
+        parsedComp = component; 
+    }
+
+    return parsedComp;
+
+}
+
+function injector(html,components){
+
+    return html.replace(componentReg, function(component){
+        var componentName = component.match(componentNameReg)[0];
+        var arguments;
+
+        var argsArr = component.match(argumentReg);
+
+        if(argsArr){
+            var argsString = `{ ${argsArr.map(item=>item.replace('=',':')).join(',')} }`
+            arguments = eval(`(${argsString})`);
+        };
+
+        if(components[componentName]){
+            return injectArgs(components[componentName],arguments);
+        }
     });
 }
 
@@ -19,12 +52,12 @@ module.exports = function (html) {
     const components = {};
 
     fs.readdirSync(path).map(file=>{
-        const componentName = `<${file.replace('.html','')}/>`;
+        const componentName = file.replace('.html','');
         const content = fs.readFileSync(`${path}/${file}`,'utf8');
 
         components[componentName] = content;
     });
 
 
-    return replaceAll(html,components);
+    return injector(html,components);
 }
