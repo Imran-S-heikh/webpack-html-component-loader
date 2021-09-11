ecfa6122c1cf160437923172a237b685a3900ad5
Imran-S-heikh <imransheikhsadi@gmail.com>
2021-09-11 13:44:33 +0600

    Manual Compile Problem Solved and Auto reload added with hack

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+const { job, start, stop } = require("microjob");
 const loader_utils = require('loader-utils');
 const fs = require('fs').promises;
-const xFs = require('fs');
 const chokidar = require('chokidar');
 const { transformAsync } = require("@babel/core");
 const reactPreset = require('@babel/preset-react');
 const xPath = require('path');
-const vm = require('vm');
+const { clear } = require("console");
 
 
 const componentReg = new RegExp(/<[A-Z]([^\/>]*)+\s?\/>/, "g");
@@ -19,76 +19,79 @@ const babalConf = {
     presets: ['@babel/preset-env', reactPreset]
 }
 const components = {};
-const compDirPath = xPath.resolve(__dirname,compDir);
+const compDirPath = xPath.resolve(__dirname, compDir);
+let config = null;
 
 let init = true;
 
-// function injectArgs(component,args) {
-//     var parsedComp;
+async function reloadPage(componentName) {
 
-//     if(args){
-//         parsedComp = component.replace(templateReg,(match)=>{
-//             var key = match.replace('{','').replace('}','');
-//             if(args[key]){
-//                 return args[key]
-//             }
-//         });
-//     }else{
-//         parsedComp = component; 
-//     }
+    const htmlFileDir = config.reload.dir || './src';
+    const files = (await fs.readdir(htmlFileDir)).filter(e=>e.includes('.html'));
 
-//     return parsedComp;
+    if(currentServingPath === '/'){
 
-// }
+        fs.appendFile(`${htmlFileDir}/index.html`,' ');
 
-function compExe(code) {
-    const context = { html: '',require,exports,console}
+    }else if (currentServingPath) {
+        fs.appendFile(`${htmlFileDir}/${currentServingPath}`,' ');
+
+    }else if(files.includes(`${componentName}.html`)) {
+
+        fs.appendFile(`${htmlFileDir}/${componentName}.html`,' ');
+
+    }else{
+        // Logic for itarating over files and change all
+    }
+}
+
+async function replaceAsync(str, regex, asyncFn) {
+    const promises = [];
+    str.replace(regex, (match, ...args) => {
+        const promise = asyncFn(match, ...args);
+        promises.push(promise);
+    });
+    const data = await Promise.all(promises);
+    return str.replace(regex, () => data.shift());
+}
+
+function genRectTohtml(code) {
+    const vm = require('vm');
+    const context = { html: '', require, exports };
 
     const script = new vm.Script(code);
     vm.createContext(context);
-    script.runInNewContext(context);
+    script.runInContext(context);
 
-    return context.html;
+    return context.html
 }
 
-function injector(html) {
+async function componentHandler(component) {
+    const componentName = component.match(componentNameReg)[0];
+    let htmlComponent = '';
 
-    return html.replace(componentReg, function (component) {
-        var componentName = component.match(componentNameReg)[0];
-
-        // console.log(`${compDir}/${componentName}.js`);
-
-        const code = `
+    const code = `
             const ReactDOMServer = require('react-dom/server');
             const fs = require('fs');
 
             if(fs.existsSync('${compDirPath}/${componentName}.js')){
                 const ${componentName} = require('${compDirPath}/${componentName}').default;
                 html = ReactDOMServer.renderToStaticMarkup(${componentName}());
-                console.log(html)
             }
         `;
 
 
-        // if (components[componentName]) {
-            console.log(`'${compDirPath}/${componentName}.js'`);
-            return compExe(code);
-        // }
-        // var arguments;
+    try {
+        await start({ maxWorkers: 4 });
+        htmlComponent = await job(genRectTohtml,{data: code});
 
-        // var argsArr = component.match(argumentReg);
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await stop();
+    }
 
-        // if(argsArr){
-        //     var argsString = `{ ${argsArr.map(item=>item.replace('=',':')).join(',')} }`
-        //     arguments = eval(`(${argsString})`);
-        // };
-
-        // if(components[componentName]){
-        //     return injectArgs(components[componentName],arguments);
-        // }
-        console.log(component)
-        return component
-    });
+    return htmlComponent
 }
 
 
@@ -116,47 +119,45 @@ async function transpile(esNext, path) {
     es15 && writeFile(path, es15.code);
 }
 
-function writeFile(path, data) {
+async function writeFile(path, data) {
 
-    const name = path.split('/').slice(-1)[0].split('.')[0];
+    const fileName = path.split('/').slice(-1)[0];
+    const name = fileName.split('.')[0];
     components[name] = {
-        es15: xPath.resolve(__dirname,`./${compDir}/${name}.js`),
+        es15: xPath.resolve(__dirname, `./${compDir}/${name}.js`),
         esNext: path
     }
 
-    console.log(components);
 
-    fs.writeFile(xPath.resolve(__dirname,`${compDir}/${name}.js`), data);
+    await fs.writeFile(xPath.resolve(__dirname, `${compDir}/${name}.js`), data);
+    if (config.reload && config.reload.status === true) {
+        reloadPage(name);
+    }
+}
+
+async function clearOldComponents() {
+    await fs.rm(compDirPath,{
+        recursive: true
+    });
+
+    fs.mkdir(compDirPath);
 }
 
 module.exports = function (html) {
 
-    const config = loader_utils.getOptions(this);
+    const callback = this.async();
+    config = loader_utils.getOptions(this);
 
     if (init && config.componentPath) {
         watchComponents(config.componentPath);
+        clearOldComponents();
         init = false;
     }
 
-    // console.log(xFs.existsSync('compiled','This Directory exists'));
+    (async function () {
+        const newHtml = await replaceAsync(html, componentReg, componentHandler);
+        callback(null,newHtml)
+    })();
 
-    xFs.readdir(xPath.resolve(__dirname,compDir),'utf-8',(err,data)=>{
-        console.log(data);
-    });
-
-
-
-
-
-
-
-
-
-    // create object of the compontents { component: '{conponent content}' }
-    // if (currentServingPath) {
-    // console.log(currentServingPath,'-x-x-x-x-x--xx------x------xx----x-------------x---xx-------xx-x---x--------------')
-
-    console.log(html)
-
-    return injector(html);
+    
 }
